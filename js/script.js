c67ba8966af806b61a6b538c9d4afd6125122c5b
capy-wave <peww.kay@gmail.com>
2024-11-17 21:23:20 -0800

    prueba

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('guest-button').addEventListener('click', () => {
-    alert("Ingresaste como invitado");
-    window.location.href = "pagina-invitado.html"; // Redirige a una página de invitado
-});
